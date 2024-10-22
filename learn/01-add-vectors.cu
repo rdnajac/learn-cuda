936fc0b6d93f562a09084fe6ab0922a549dc65de
Ryan D. Najac <ryan.najac@columbia.edu>
2024-10-22 00:31:53 -0400

    grid-stride loop

--- learn/01-add-vectors.cu
+++ learn/01-add-vectors.cu
@@ -4,8 +4,8 @@
 // Kernel function to add the elements of two arrays
 __global__ void add(int n, float *x, float *y)
 {
-    int index = threadIdx.x;
-    int stride = blockDim.x;
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
     for (int i = index; i < n; i += stride)
         y[i] = x[i] + y[i];
 }
